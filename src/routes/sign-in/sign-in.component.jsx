bceb5a1d6637ae0d29bf37182484eb2f4c097b7a
HoangLeCoder <hle070302@gmail.com>
2023-07-20 10:43:47 +0700

    Complete Google Sign In with Firebase

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,18 +1,40 @@
-import { signInWithGooglePopup, createUserDocumentFromAuth } from "../../utils/firebase/firebase.utils";
+import { useEffect } from "react";
+import { getRedirectResult } from "firebase/auth";
 
+import {
+    auth, 
+    signInWithGooglePopup,
+    signInWithGoogleRedirect,
+    createUserDocumentFromAuth,
+} from "../../utils/firebase/firebase.utils";
 
 const SignIn = () => {
+    const getRedirect = async () => {
+        const response = await getRedirectResult(auth);
+
+        if (response) {
+            const userDocRef = await createUserDocumentFromAuth(response.user);
+        }
+    }
+    
+    useEffect(() => {
+        getRedirect();
+    }, []);
+
     const logGoogleUser = async () => {
-        const {user} = await signInWithGooglePopup();
+        const { user } = await signInWithGooglePopup();
         const userDocRef = await createUserDocumentFromAuth(user);
     };
 
     return (
         <div>
             <h1>SignIn</h1>
-            <button onClick={logGoogleUser}>Sign in with Google</button>
+            <button onClick={logGoogleUser}>Sign in with Google Popup</button>
+            <button onClick={signInWithGoogleRedirect}>
+                Sign in with Google Redirect
+            </button>
         </div>
     );
 };
 
-export default SignIn;
+export default SignIn
